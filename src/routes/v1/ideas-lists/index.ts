2bc79e222cdcc3fb59a76be4bb4230b17db7daf3
ilialksv <iliaalekseevofb@gmail.com>
2025-10-28 05:47:07 +0500

    fix: updated ideas list api folder structure

--- src/routes/v1/ideas-lists/index.ts
+++ src/routes/v1/ideas-lists/index.ts
@@ -1,7 +1,7 @@
-export { deleteIdeasListRoute } from "./ideas-list/delete/route";
-export { getIdeasListRoute } from "./ideas-list/get/route";
 export { getIdeasRoute } from "./ideas-list/ideas/get/route";
 export { createIdeaRoute } from "./ideas-list/ideas/post/route";
-export { updateIdeasListRoute } from "./ideas-list/patch/route";
+export { deleteIdeasListRoute } from "./ideas-list/root/delete/route";
+export { getIdeasListRoute } from "./ideas-list/root/get/route";
+export { updateIdeasListRoute } from "./ideas-list/root/patch/route";
 export { getMyIdeasListsRoute } from "./my/get/route";
 export { createIdeasListRoute } from "./root/post/route";
