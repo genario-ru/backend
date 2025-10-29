5d1c737ba4d14d9f4d8b596c860b548014e8a96e
ilialksv <iliaalekseevofb@gmail.com>
2025-10-29 05:02:43 +0500

    refactor: updated scenarios handlers folder structure

--- src/routes/v1/scenarios/index.ts
+++ src/routes/v1/scenarios/index.ts
@@ -1,6 +1,6 @@
 export { getMyScenariosRoute } from "./my/get/route";
 export { createScenarioRoute } from "./root/post/route";
 export { updateScenarioCurrentVersionRoute } from "./scenario/current-version/patch/route";
-export { deleteScenarioRoute } from "./scenario/delete/route";
-export { getScenarioRoute } from "./scenario/get/route";
-export { updateScenarioRoute } from "./scenario/patch/route";
+export { deleteScenarioRoute } from "./scenario/root/delete/route";
+export { getScenarioRoute } from "./scenario/root/get/route";
+export { updateScenarioRoute } from "./scenario/root/patch/route";
