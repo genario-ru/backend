de044e950a29a5fd98df16c5e545eb65db56853a
ilialksv <iliaalekseevofb@gmail.com>
2026-01-07 01:02:18 +0800

    feat: updated defaults

--- src/constants/api/defaults.ts
+++ src/constants/api/defaults.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_PAGE = 1;
-export const DEFAULT_PER_PAGE = 10;
+export const DEFAULT_PER_PAGE = 32;
 export const DEFAULT_SORT_BY = "createdAt" as const;
 export const DEFAULT_SORT_ORDER = "desc" as const;
