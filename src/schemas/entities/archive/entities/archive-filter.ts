21fe005fea5bd36b43b76facb92a9273f60e6e81
ilialksv <iliaalekseevofb@gmail.com>
2026-01-09 00:17:05 +0800

    feat: simplified schema

--- src/schemas/entities/archive/entities/archive-filter.ts
+++ src/schemas/entities/archive/entities/archive-filter.ts
@@ -28,41 +28,14 @@ export const archiveFilterOptionSchema = z
 
 export type ArchiveFilterOption = z.infer<typeof archiveFilterOptionSchema>;
 
-const archiveFilterBaseSchema = z.object({
-  slug: z.enum(ARCHIVE_FILTER_IDS),
-  name: z.string(),
-  icon: z.string().nullable(),
-  options: z.array(archiveFilterOptionSchema),
-});
-
-export const archiveSelectFilterSchema = archiveFilterBaseSchema
-  .extend({
-    type: z.literal("select"),
+const archiveFilterSchema = z
+  .object({
+    slug: z.enum(ARCHIVE_FILTER_IDS),
+    name: z.string(),
+    icon: z.string().nullable(),
+    type: z.enum(["select", "multiselect"]),
+    options: z.array(archiveFilterOptionSchema),
   })
-  .register(archiveRegistry, {
-    title: "Archive select filter",
-    description: "Фильтр с одиночным выбором",
-    ref: "ArchiveSelectFilterSchema",
-  });
-
-export type ArchiveSelectFilter = z.infer<typeof archiveSelectFilterSchema>;
-
-export const archiveMultiSelectFilterSchema = archiveFilterBaseSchema
-  .extend({
-    type: z.literal("multiselect"),
-  })
-  .register(archiveRegistry, {
-    title: "Archive multiselect filter",
-    description: "Фильтр с множественным выбором",
-    ref: "ArchiveMultiSelectFilterSchema",
-  });
-
-export type ArchiveMultiSelectFilter = z.infer<
-  typeof archiveMultiSelectFilterSchema
->;
-
-export const archiveFilterSchema = z
-  .union([archiveSelectFilterSchema, archiveMultiSelectFilterSchema])
   .register(archiveRegistry, {
     title: "Archive filter",
     description: "Фильтр архива",
