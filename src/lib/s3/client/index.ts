55ed7b2a4b45812c865962b49a49b76c5297673c
ilialksv <iliaalekseevofb@gmail.com>
2026-02-21 22:21:23 +0500

    fix: fixed s3 client

--- src/lib/s3/client/index.ts
+++ src/lib/s3/client/index.ts
@@ -3,6 +3,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { envs } from "@/constants/common/envs";
 
 export const s3 = new S3Client({
+  endpoint: envs.S3_BUCKET_BASE_URL,
   region: envs.S3_REGION,
   credentials: {
     accessKeyId: envs.S3_ACCESS_KEY,
