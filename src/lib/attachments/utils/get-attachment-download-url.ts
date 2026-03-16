a72f93c3db66422c91547954e5d5612630f1f21c
ilialksv <iliaalekseevofb@gmail.com>
2026-03-17 00:48:24 +0500

    feat: updated documentUrl to not have host

--- src/lib/attachments/utils/get-attachment-download-url.ts
+++ src/lib/attachments/utils/get-attachment-download-url.ts
@@ -1,8 +1,3 @@
-import { envs } from "@/constants/common/envs";
-
 export function getAttachmentDownloadUrl(attachmentId: string): string {
-  return new URL(
-    `/api/v1/attachments/${attachmentId}/download`,
-    envs.BACKEND_BASE_URL,
-  ).toString();
+  return `/api/v1/attachments/${attachmentId}/download`;
 }
