e64e6c955ff56c3b763233ee484fa3be7f681984
ilialksv <iliaalekseevofb@gmail.com>
2026-04-05 03:31:16 +0500

    feat: removed hide in production flag

--- src/middleware/openapi-response-middleware.ts
+++ src/middleware/openapi-response-middleware.ts
@@ -1,6 +1,5 @@
 import { describeRoute } from "hono-openapi";
 
-import { envs } from "@/constants/common/envs";
 import { errorResponses } from "@/constants/openapi/error-responses";
 import type { OpenAPIResponses } from "@/types/openapi/openapi-responses";
 
@@ -8,19 +7,16 @@ type OpenAPIResponseMiddlewareParams = {
   tags: string[];
   responses: OpenAPIResponses;
   withoutDefaultResponses?: boolean;
-  hideInProduction?: boolean;
 };
 
 export const openAPIResponseMiddleware = ({
   tags,
   responses,
   withoutDefaultResponses = false,
-  hideInProduction = false,
 }: OpenAPIResponseMiddlewareParams) => {
   const defaultResponses = withoutDefaultResponses ? {} : errorResponses;
 
   return describeRoute({
-    hide: hideInProduction && envs.NODE_ENV === "production",
     tags,
     responses: {
       ...defaultResponses,
