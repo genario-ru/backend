de0cd3eba42dc94031f6aa9721894ab7a77ed914
ilialksv <iliaalekseevofb@gmail.com>
2026-05-18 05:25:08 +0500

    fix: fixed better auth zod schema errors

--- src/domains/auth/schemas/entities/session.ts
+++ src/domains/auth/schemas/entities/session.ts
@@ -3,10 +3,18 @@ import { createSelectSchema } from "drizzle-zod";
 import { session } from "@/db/schema";
 import { z } from "@/lib/zod";
 
-export const authSessionSchema = createSelectSchema(session).meta({
-  title: "Auth session",
-  description: "Auth session description",
-  ref: "AuthSessionSchema",
-});
+export const authSessionSchema = createSelectSchema(session)
+  .extend({
+    id: z.string(),
+    impersonatedBy: z.string().nullish(),
+    expiresAt: z.coerce.string(),
+    createdAt: z.coerce.string(),
+    updatedAt: z.coerce.string(),
+  })
+  .meta({
+    title: "Auth session",
+    description: "Auth session description",
+    ref: "AuthSessionSchema",
+  });
 
 export type AuthSession = z.infer<typeof authSessionSchema>;
--- src/domains/auth/schemas/entities/user.ts
+++ src/domains/auth/schemas/entities/user.ts
@@ -3,10 +3,20 @@ import { createSelectSchema } from "drizzle-zod";
 import { user } from "@/db/schema";
 import { z } from "@/lib/zod";
 
-export const userSchema = createSelectSchema(user).meta({
-  title: "User",
-  description: "User description",
-  ref: "UserSchema",
-});
+export const userSchema = createSelectSchema(user)
+  .extend({
+    phone: z.string().nullish(),
+    phoneVerified: z.boolean().optional().default(false),
+    image: z.string().nullish(),
+    banReason: z.string().nullish(),
+    banExpires: z.coerce.string().nullish(),
+    updatedAt: z.coerce.string(),
+    createdAt: z.coerce.string(),
+  })
+  .meta({
+    title: "User",
+    description: "User description",
+    ref: "UserSchema",
+  });
 
 export type User = z.infer<typeof userSchema>;
