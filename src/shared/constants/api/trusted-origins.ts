4f8393b5605febd2e259c92c6f366e3509347abe
ilialksv <iliaalekseevofb@gmail.com>
2026-05-24 18:18:26 +0500

    feat: updated trusted origins

--- src/shared/constants/api/trusted-origins.ts
+++ src/shared/constants/api/trusted-origins.ts
@@ -1,11 +1,3 @@
 import { env } from "@/env";
 
-const FRONTEND_LOCAL_BASE_URL = "http://localhost:5173";
-const BACKEND_LOCAL_BASE_URL = "http://localhost:3000";
-
-export const TRUSTED_ORIGINS = [
-  env.FRONTEND_BASE_URL,
-  env.BACKEND_BASE_URL,
-  FRONTEND_LOCAL_BASE_URL,
-  BACKEND_LOCAL_BASE_URL,
-];
+export const TRUSTED_ORIGINS = [env.FRONTEND_BASE_URL, env.BACKEND_BASE_URL];
